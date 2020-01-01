e7d6ec725bb5f0e00a1f226bcf76b7956f03d1b4
JoaoGomees <36642833+JoaoGomees@users.noreply.github.com>
2020-01-01 14:14:46 +0000

    Update Music.java

--- src/Music.java
+++ src/Music.java
@@ -1,24 +1,31 @@
 
 public class Music {
 
+	private int identificador;
 	private String nome;
 	private String autor;
 	private int duration; //em segundos
 	private int ano;
 	private String [] categorias;
 	private int numero_downloads;
+	private String file_path;
 	
-	public Music (String nome, String autor, int duration, int ano, String [] categorias) {
+	public Music (int id, String nome, String autor, int duration, int ano, String [] categorias, String file_path) {
+		this.identificador = id;
 		this.nome = nome;
 		this.autor = autor;
 		this.duration = duration;
 		this.ano = ano;
 		this.numero_downloads = 0;
+		this.file_path = file_path;
 		
 		for (int i = 0; i < categorias.length; i++) 
 			this.categorias [i] = categorias [i];
 	}
 	
+	public int get_id () {
+		return this.identificador;
+	}
 	
 	public String get_nome () {
 		return this.nome;
@@ -44,7 +51,18 @@ public class Music {
 		return this.numero_downloads;
 	}
 	
+	public String get_filePath () {
+		return this.file_path;
+	}
+	
 	public void inc_numeroDownloads () {
 		this.numero_downloads++;
+	}
+	
+	public String to_String () {
+		StringBuilder sb = null;
+		sb.append("musica:" + this.get_id() + ":" + this.get_nome() + ":" + this.get_autor());
+		
+		return sb.toString();
 	}
 }
