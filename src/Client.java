651b0d9e0274bcd36223e64f38ab1455abb94cb6
JoaoGomees <36642833+JoaoGomees@users.noreply.github.com>
2020-01-01 14:15:31 +0000

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,47 +1,64 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
 public class Client {
 	
 	private Socket clientSocket;
+	private BufferedReader buffer;
 	private BufferedReader in;
 	private PrintWriter out;
-	private Scanner sc;
 	
     public Client (String host, int port) throws UnknownHostException, IOException {
     	this.clientSocket = new Socket (host,port);
-    	this.sc = new Scanner (System.in);
+    	this.buffer = new BufferedReader(new InputStreamReader (System.in));
+
     }
     
     public void start () throws IOException {
     	this.in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
     	this.out = new PrintWriter (this.clientSocket.getOutputStream());
     	
-    	while (true) {
-    		String input = sc.nextLine();
+    	while (true ) {
     		
-    		if (!input.equals("Quit")) {
-    			out.println(input);
-        		out.flush();
-        		System.out.println (in.readLine());
+    		String input = buffer.readLine();
+    		String parser [] = input.trim().split(":");
+    		this.out.println(input);
+    		this.out.flush();
+    		
+    		if ("quit".equals(parser[0]) || parser[0] == null)
+    			break;
+    		
+    		else if ("create account".equals(parser[0])) 
+    			System.out.println(in.readLine());
+    		
+    		else if ("upload".equals(parser[0])) {
+    			System.out.println("Here");
+				File file = new File(parser[1]);
+				long length = file.length();
+				byte[] bytes = new byte[1000000];
+				InputStream in = new FileInputStream(file);
+				OutputStream out = this.clientSocket.getOutputStream();
+				int count;
+	
+				System.out.println("Sending");
+				while ((count = in.read(bytes)) > 0) {
+					out.write(bytes, 0, count);
+				}
+				out.close();
+				in.close();
+				break;
     		}
-    		
-    		else this.stop();
     	}
     }
     
-    public void stop () throws IOException {
-    	this.clientSocket.close();
-    	this.in.close();
-    	this.out.close();
-    	this.sc.close();
-    }
     
     public static void main (String args []) throws UnknownHostException, IOException {
 		Client client = new Client ("127.0.0.1", 6666);
@@ -50,5 +67,3 @@ public class Client {
 	}
     
 }
-
-	
