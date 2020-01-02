c50b5af4200ed8342cc8106b54747a9fe3c78a8c
JoaoGomees <36642833+JoaoGomees@users.noreply.github.com>
2020-01-02 12:06:28 +0000

    Service sem o lixo dos if's todos

--- src/Service.java
+++ src/Service.java
@@ -20,47 +20,46 @@ public class Service implements Runnable {
 	}
 	
 	public void run () {
-		String answer;
 		
 		try {
+			
 			BufferedReader in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
 			PrintWriter out = new PrintWriter(this.clientSocket.getOutputStream());
 			String s;
+			
 			while ((s = in.readLine()) != null) {
+				
 				System.out.println("lido: " + s);
 				String p = s.trim();
 				String parser [] = p.split(":");
+				int parser_size = parser.length;
 				
-				if ("create account".equals(parser[0])) {
-					this.biblioteca.createAccount(parser[1], parser[2]);
-					out.println("Conta com email: " + parser[1] + " criada\n");
-					out.flush();
-				}
+				switch (parser[0]) {
+					case "create account": 	this.biblioteca.createAccount(parser[1], parser[2]);
+											out.println("Conta com email: " + parser[1] + " criada\n");
+											out.flush();
+											
+					case "upload":	 InputStream inS = this.clientSocket.getInputStream();
+					 				File f = new File(parser[1]);
+					 				String fname = f.getName();
+					 				OutputStream outS = new FileOutputStream(new File ("music/" + fname + ".xml"));
+					 				byte[] bytes = new byte[1000000];
 				
-				else if ("upload".equals(parser[0])){
-					 InputStream inS = this.clientSocket.getInputStream();
-					 File f = new File(parser[1]);
-					 String fname = f.getName();
-					 OutputStream outS = new FileOutputStream(new File ("music/" + fname + ".xml"));
-					 byte[] bytes = new byte[1000000];
-				
-					 int count;
-					 System.out.println ("Receiving");
-				        while ((count = inS.read(bytes)) > 0) {
-				            outS.write(bytes, 0, count);
-				        }
+					 				int count;
+					 				System.out.println ("Receiving");
+					 				while ((count = inS.read(bytes)) > 0) {
+					 					outS.write(bytes, 0, count);
+					 				}
 				        
-				     System.out.println("Received");
+					 				System.out.println("Received");
 				        
-				     outS.close();
-				     inS.close();
-				}
+					 				outS.close();
+					 				inS.close();
+					}	
 				
 			}
 			
-			
-			
-		} catch (IOException e) {
+		}catch (IOException e) {
 			e.printStackTrace();
 		}
 		
